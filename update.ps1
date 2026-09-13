5b021d16420332c856de35c75c5786645c8a7f33
brogers5 <6869577+brogers5@users.noreply.github.com>
2026-09-13 09:45:00 -0400

    Enable Stable channel

--- update.ps1
+++ update.ps1
@@ -13,8 +13,7 @@ function global:au_GetLatest {
     $streams = [ordered] @{
         ReleaseCandidateWinRing0 = Get-LatestReleaseCandidateVersionInfo -WinRing0
         ReleaseCandidate         = Get-LatestReleaseCandidateVersionInfo
-        #Temporarily disabled, since the latest Stable release currently doesn't have an installer
-        # Stable                   = Get-LatestStableVersionInfo
+        Stable                   = Get-LatestStableVersionInfo
     }
 
     return @{ Streams = $streams }
