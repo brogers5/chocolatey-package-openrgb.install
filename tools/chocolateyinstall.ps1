0b6f638747fccb104144ea815f72d93113d39261
brogers5 <6869577+brogers5@users.noreply.github.com>
2026-09-13 09:45:45 -0400

    Update to 1.0.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $softwareNamePattern = 'OpenRGB'
 
-$fileName = 'OpenRGB_1.0rc3.1_Windows_64_5e81e26.msi'
+$fileName = 'OpenRGB_1.0_Windows_64_81bbe18.msi'
 $filePath = Join-Path -Path $toolsDir -ChildPath $fileName
 
 $packageArgs = @{
